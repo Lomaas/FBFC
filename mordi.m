24fc6f347d4730a9096d4419242a07e190f8db8c
Simen Johannessen <simenlomaas@gmail.com>
2014-10-26 22:10:50 +0100

    local storage

--- mordi.m
+++
@@ -1,9 +0,0 @@
-//
-//  mordi.m
-//  FBcleaner
-//
-//  Created by Simen Johannessen on 21/10/14.
-//  Copyright (c) 2014 Simen Johannessen. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
