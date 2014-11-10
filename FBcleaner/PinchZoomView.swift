ed957a7c0869b2dd3114ec180e657ff4a30943fb
Simen Johannessen <simenlomaas@gmail.com>
2014-11-10 23:19:56 +0100

    updates

---
+++ FBcleaner/PinchZoomView.swift
@@ -0,0 +1,73 @@
+//
+//  PinchZoomView.swift
+//  FBcleaner
+//
+//  Created by Simen Johannessen on 10/11/14.
+//  Copyright (c) 2014 Simen Johannessen. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PinchZoomView: UIView, UIScrollViewDelegate {
+    var scrollView: UIScrollView
+    var uiImageView: UIImageView
+    
+    init(imageView: UIImageView, frame: CGRect){
+        self.uiImageView = imageView.c
+        self.uiImageView.frame = frame
+        self.scrollView = UIScrollView(frame: frame)
+        self.scrollView.minimumZoomScale = 1.0;
+        self.scrollView.maximumZoomScale = 6.5;
+        self.scrollView.contentSize = frame.size
+        super.init(frame: frame)
+        self.scrollView.delegate = self;
+        self.scrollView.addSubview(self.uiImageView)
+        self.addSubview(self.scrollView)
+        self.backgroundColor = UIColor.blackColor()
+        
+        var doubleTap = UITapGestureRecognizer(target: self, action: "handleDoubleTap:")
+        doubleTap.numberOfTapsRequired = 2
+        self.scrollView.addGestureRecognizer(doubleTap)
+        
+        var singleTap = UITapGestureRecognizer(target: self, action: "singleTapButton:")
+        singleTap.numberOfTapsRequired = 1
+        var button = UIButton(frame: CGRectMake(3, 19, 60, 30))
+        button.setTitle("back", forState: UIControlState.Normal)
+        button.addGestureRecognizer(singleTap)
+        button.layer.borderColor = UIColor.whiteColor().CGColor
+        button.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+        button.layer.borderWidth = 2.0
+        button.layer.cornerRadius = 3.0
+        self.addSubview(button)
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("not implementeed")
+    }
+    
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        return self.uiImageView
+    }
+    
+    func singleTapButton(recognizer: UITapGestureRecognizer){
+        self.removeFromSuperview()
+    }
+
+    func handleDoubleTap(recognizer: UITapGestureRecognizer){
+        let pointInView = recognizer.locationInView(self.uiImageView)
+        
+        var newZoomScale = self.scrollView.zoomScale * 1.5
+        newZoomScale = min(newZoomScale, self.scrollView.maximumZoomScale)
+        
+        let scrollViewSize = self.scrollView.bounds.size
+        let w = scrollViewSize.width / newZoomScale
+        let h = scrollViewSize.height / newZoomScale
+        let x = pointInView.x - (w / 2.0)
+        let y = pointInView.y - (h / 2.0)
+        
+        let rectToZoomTo = CGRectMake(x, y, w, h);
+        
+        scrollView.zoomToRect(rectToZoomTo, animated: true)
+    }
+}
