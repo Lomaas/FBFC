ede2b33c4f62fa6c7e86a6053087965d3a828bcf
Simen Johannessen <simenlomaas@gmail.com>
2014-11-24 20:05:57 +0100

    update

--- FBcleaner/GlobalConstans.swift
+++ FBcleaner/GlobalConstans.swift
@@ -23,5 +23,7 @@ let BLUE_COLOR = UIColor(red: 38, green: 100, blue: 255, alpha: 100)
 let GREEN_COLOR = UIColor(red: 83/255, green: 153/255, blue: 93/255, alpha: 1.0)
 let RED_COLOR = UIColor(red: 224, green: 0.3, blue:0.2, alpha: 1.0)
 let ANIMATION_TIME = 0.3
-let BACKGROUND_COLOR = UIColor(red: 263/255, green: 245/255, blue: 230/255, alpha: 1.0)
+//let BACKGROUND_COLOR = UIColor(red: 263/255, green: 245/255, blue: 230/255, alpha: 1.0)
+let BACKGROUND_COLOR = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1.0)
+
 let IMAGE_LAYER_COLOR = UIColor.lightGrayColor()
